591eb7141e9835b774fda004231a9f0c8a84a2c7
Ziru Zou <jiyucoding@yahoo.com>
2023-02-06 00:19:51 -0500

    cresate a useEffect, use setTimeout to run a CB which decrease state by 1 after 1s

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  // Deliverable 1: 
+  // create a useEffect, use setTimeout to run a CB after 1s
+  // CB update timeRemaining by decreasing 1 each second
+  // pass timeRemaining into 2nd dependency
+  useEffect(() => {
+    setTimeout(() => setTimeRemaining(timeRemaining - 1), 1000)  
+  }, [timeRemaining])
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
